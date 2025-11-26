c37ebc5808b8035675845872b95ea2aecf689889
Nalin-khanna <nalinkhanna17@gmail.com>
2025-11-27 01:59:23 +0530

    added database logic

---
+++ src/database-integrations/postgres/index.ts
@@ -0,0 +1,125 @@
+import { Client } from 'pg'
+import DodoPayments from 'dodopayments';
+
+let pgClient : Client
+
+const ConnectPostgres = async (uri : string) => {
+    try {
+        pgClient = new Client({
+            connectionString : uri
+        });
+        await pgClient.connect();
+    } catch (error){
+        await initTables();
+    }
+}
+
+const initTables = async () => {
+    // I have used JSONB to store the data
+    const tableQueries = [
+        `CREATE TABLE IF NOT EXISTS Subscription (
+            id TEXT PRIMARY KEY,
+            data JSONB NOT NULL
+        );`,
+        `CREATE TABLE IF NOT EXISTS Payment (
+            id TEXT PRIMARY KEY,
+            data JSONB NOT NULL,
+        );`,
+        `CREATE TABLE IF NOT EXISTS License (
+            id TEXT PRIMARY KEY,
+            data JSONB NOT NULL,
+        );`,
+        `CREATE TABLE IF NOT EXISTS Customer (
+            id TEXT PRIMARY KEY,
+            data JSONB NOT NULL,
+        );`
+    ];
+    for (const query of tableQueries) {
+        await pgClient.query(query);
+    }
+}
+
+async function AddSubscriptionPostgres(subscriptionData: DodoPayments.Subscriptions.SubscriptionListResponse) {
+    const query = `
+        INSERT INTO Subscription (id, data)
+        VALUES ($1, $2)
+        ON CONFLICT (id) DO UPDATE SET
+        id = EXCLUDED.id,
+        data = EXCLUDED.data;
+    `;
+
+    const values = [
+        subscriptionData.subscription_id,
+        JSON.stringify(subscriptionData)
+    ];
+
+    try {
+        await pgClient.query(query, values);
+    } catch (error) {
+        console.error(`Error syncing subscription ${subscriptionData.subscription_id}:`, error);
+        throw error;
+    }
+}
+
+async function AddPaymentPostgres(paymentData: DodoPayments.Payments.PaymentListResponse){
+    const query = `
+        INSERT INTO Payment (id, data)
+        VALUES ($1, $2)
+        ON CONFLICT (id) DO UPDATE SET
+        id = EXCLUDED.id,
+        data = EXCLUDED.data;
+    `;
+
+    const values = [
+        paymentData.payment_id,
+        JSON.stringify(paymentData)
+    ]
+    try{
+        await pgClient.query(query,values)
+    }catch(error){
+        console.error(`Error syncing subscription ${paymentData.subscription_id}:`, error);
+        throw error;
+    }
+}
+
+async function AddLicencePostgres(licenceData: DodoPayments.LicenseKeys.LicenseKey){
+    const query = `
+        INSERT INTO License (id, data)
+        VALUES ($1, $2)
+        ON CONFLICT (id) DO UPDATE SET
+        id = EXCLUDED.id,
+        data = EXCLUDED.data;
+    `;
+    const values = [
+        licenceData.subscription_id,
+        JSON.stringify(licenceData)
+    ]
+    try{
+        await pgClient.query(query,values)
+    }catch(error){
+        console.error(`Error syncing subscription ${licenceData.subscription_id}:`, error);
+        throw error;
+    }
+}
+
+async function AddCustomerPostgres(customerData: DodoPayments.Customers.Customer){
+    const query = `
+        INSERT INTO Customer (id, data)
+        VALUES ($1, $2)
+        ON CONFLICT (id) DO UPDATE SET
+        id = EXCLUDED.id,
+        data = EXCLUDED.data;
+    `;
+    const values = [
+        customerData.customer_id,
+        JSON.stringify(customerData)
+    ]
+    try{
+        await pgClient.query(query,values)
+    }catch(error){
+        console.error(`Error syncing subscription ${customerData.customer_id}:`, error);
+        throw error;
+    }
+}
+
+export { ConnectMongoDB, AddSubscriptionMongoDB, AddPaymentMongoDB, AddLicenceMongoDB, AddCustomerMongoDB };
